5813d2fcc0a0222f3be58b646bc4a2e3ccc2d347
ThanhTai98 <hothanhtai161998@gmail.com>
2022-11-11 20:05:07 +0700

    Dựng Header ( logo, search )

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,7 +1,15 @@
 import classNames from 'classnames/bind';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //
 import styles from './Header.module.scss';
+
+//import logo
+import images from '~/assets/images';
+import {
+  faCircleXmark,
+  faSearch,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -10,8 +18,23 @@ function Header() {
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         {/* Logo */}
+        <div className={cx('logo')}>
+          <img src={images.logo} alt='Tik-tok' />
+        </div>
         {/* search */}
+        <div className={cx('search')}>
+          <input placeholder='Search accounts and video' spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          {/* loading */}
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
         {/* navbar */}
+        <div className={cx('action')}></div>
       </div>
     </header>
   );
